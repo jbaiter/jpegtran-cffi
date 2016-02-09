566ab5972ae8dc44ed1424ddd3380e602f10211e
Johannes Baiter <johannes.baiter@gmail.com>
2016-02-09 15:08:20 +0100

    Update Travis config

--- .setup_travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ $JPEGLIB -eq "jpeg8" ]; then
-    apt-get -y remove libjpeg-turbo8-dev
-    apt-get -y install libjpeg8-dev
-elif [ $JPEGLIB -eq "turbojpeg" ]; then
-    apt-get -y remove libjpeg8-dev
-    apt-get -y install libjpeg-turbo8-dev
-fi
